1916355638c1e3a843d7b6af3e206ff551fe84b4
AntonFartovii <anton@rza.by>
2022-11-03 17:36:48 +0300

    feat: task1 done

---
+++ 01-read-file/index.js
@@ -0,0 +1,16 @@
+import {createReadStream} from 'fs'
+import {getPathFromFiles} from "../utils/getPathFromFiles.js";
+
+
+// node 01-read-file
+export const read = async () => {
+    try {
+        const filePath = getPathFromFiles(import.meta.url, 'text.txt')
+        const stream = await createReadStream(filePath)
+        stream.pipe(process.stdout)
+    } catch (e) {
+        console.log( e )
+    }
+}
+
+await read()
