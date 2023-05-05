bc983c492bddf2088b3badbbcb23e3d78fc762fe
AntonFartovii <anton@rza.by>
2023-05-05 11:23:16 +0300

    refactor: read files-in-folder, use stat promise

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,33 +1,29 @@
 import {getDir} from "../utils/getPathFromFiles.js";
-import {join} from 'path'
-import {stat} from 'fs'
-import {readdir} from "fs/promises";
-const url = import.meta.url
+import {join, resolve} from 'path';
+import {readdir, stat} from "fs/promises";
+const url = import.meta.url;
 
 // Запустите в консоли: node 03-files-in-folder
+export const lsDir = async (folderName) => {
 
-export const lsDir = async () => {
-
-    const dirPath = getDir( url )
-    const src = join( dirPath, 'secret-folder' )
+    const dirPath = getDir(url);
+    const src = join(dirPath, folderName);
 
     try {
-        const files = await readdir( src, {withFileTypes: true})
-
-        for( const file of files)
-
-            if (file.isFile()) {
-
-                const fileStats = await stat(file)
-
-            }
-
+      const objList = await readdir(src, {withFileTypes: true});
+      for(const obj of objList)
+        if (obj.isFile()) {
+          const fileInfo = obj.name.split('.');
+          const fileStats = await stat(resolve(src, obj.name));
+          const fileSize = (fileStats.size/1024).toFixed(3)+'kb';
+          fileInfo.push(fileSize);
+          console.log(fileInfo.join(' - '));
+        }
     } catch (e) {
-        console.log( e )
+        console.log(e);
     }
 }
-
-await lsDir()
+await lsDir('secret-folder')
 
 // Данные должны быть выведены в формате <имя файла>-<расширение файла>-<вес файла>.
 // Пример: example - txt - 128.369kb
